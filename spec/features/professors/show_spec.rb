74ca41e17634223feb9c1d5a1a1282c46bffce58
Ryan Camp <campryan@comcast.net>
2020-03-27 09:54:04 -0600

    Complete professor show page and fix typo in seeds file

--- spec/features/professors/show_spec.rb
+++ spec/features/professors/show_spec.rb
@@ -4,12 +4,18 @@ RSpec.describe "As a visitor on the professor show page", type: :feature do
   it "I can see a list of student names that the professor has" do
     snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
     hagarid = Professor.create(name: "Rubus Hagarid", age: 38 , specialty: "Care of Magical Creatures")
+    lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
+
     harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
     malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
     longbottom = Student.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
-    ProfessorStudent.create(student_id: harry, professor_id: snape)
-    ProfessorStudent.create(student_id: longbottom, professor_id: snape)
-    ProfessorStudent.create(student_id: malfoy, professor_id: hagarid)
+
+    ProfessorStudent.create(student: harry, professor: snape)
+    ProfessorStudent.create(student: harry, professor: hagarid)
+    ProfessorStudent.create(student: harry, professor: lupin)
+    ProfessorStudent.create(student: malfoy, professor: hagarid)
+    ProfessorStudent.create(student: malfoy, professor: lupin)
+    ProfessorStudent.create(student: longbottom, professor: snape)
 
     visit "/professors/#{snape.id}"
 
