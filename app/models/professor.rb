edfbc18a8b2e8cf0bece6cff838250f8927f3ed3
Ryan Camp <campryan@comcast.net>
2020-03-27 10:27:58 -0600

    Create #average_student_age for Professor model

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -2,4 +2,11 @@ class Professor <ApplicationRecord
   validates_presence_of :name, :age, :specialty
   has_many :professor_students
   has_many :students, through: :professor_students
+
+  def average_student_age
+    student_ages = students.map do |student|
+      student.age
+    end
+    student_ages.sum.to_f / students.length
+  end
 end
